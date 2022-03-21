463f39f8af9db5984332605be02c9f654eb1fd47
patn237 <patrickn237@gmail.com>
2022-03-21 11:38:05 +0000

    Creted a copy of bubblereport.js

---
+++ bubblereport-copy.js
@@ -0,0 +1,28 @@
+var scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69, 34, 55, 51, 52, 44, 51, 69, 64, 66, 55, 52, 61, 46, 31, 57, 52, 44, 18, 41, 53, 55, 61, 51, 44];
+
+var highScore = 0;
+var output;
+
+for (var i = 0; i < scores.length; i++) {
+    output = "Bubble solution #" + i + " score: " + scores[i];
+    
+    console.log(output);
+
+    if (scores[i] > highScore) {
+        highScore = scores[i];
+    }
+}
+
+console.log("Bubble tests: " + scores.length);
+console.log("Highest bubble score: " + highScore);
+
+var bestSolutions = [];
+for (var i = 0; i < scores.length; i++) {
+    if (scores[i] == highScore) {
+        bestSolutions[0] = i; // Figure out a way besides using push to get the solution with the highest score.
+        bestSolutions[1] = i;
+    }
+    
+}
+
+console.log("Solutions with the highest score: " + bestSolutions);
