4d55602988a2d91da173cad86076019f065f2dee
patn237 <patrickn237@gmail.com>
2022-04-25 10:25:47 +0000

    New files

---
+++ clicker/clicker.js
@@ -0,0 +1,19 @@
+var i = 0;
+function buttonClick() {
+    i++;
+    document.getElementById('inc').value = i;
+}
+
+// function numberCounter(x) {
+//     var numCounter = document.getElementById("numbers")
+//     numCounter.innerHTML = numbers;
+// }
+
+// function clicker() {
+//     var clickButton = document.getElementById("click");
+//     if (clickButton.onclick) {
+//         numbers = numbers + 1;
+//     }
+// }
+
+// numberCounter(numbers);
---
+++ lab.js
@@ -0,0 +1,3 @@
+window.onload = function() {
+    alert("The page is loaded!");
+}
