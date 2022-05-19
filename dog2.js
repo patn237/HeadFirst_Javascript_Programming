2578f780399bf1fbe185772457b9c6dea737161d
patn237 <patrickn237@gmail.com>
2022-05-19 11:44:47 +0000

    Some more files

--- dog2.js
+++ dog2.js
@@ -33,11 +33,38 @@ Dog.prototype.sit = function() {
     }  
 };
 
+function ShowDog(name, breed, weight, handler) {
+    Dog.call(this, name, breed, weight);
+    this.handler = handler;
+}
+
+ShowDog.prototype = new Dog();
+ShowDog.prototype.constructor = ShowDog;
+
+ShowDog.prototype.league = "Webville";
+
+ShowDog.prototype.stack = function() {
+    console.log("Stack");
+};
+
+ShowDog.prototype.bait = function() {
+    console.log("Bait");
+};
+
+ShowDog.prototype.gait = function(kind) {
+    console.log(kind + "ing");
+};
+
+ShowDog.prototype.groom = function() {
+    console.log("Groom");
+};
 
 var fido = new Dog("Fido", "Mixed", 38);
 var fluffy = new Dog("Fluffy", "Poodle", 30);
 var spot = new Dog("Spot", "Chihuahua", 10);
 var barnaby = new Dog("Barnaby", "Basset Hound", 55);
+var scotty = new ShowDog("Scotty", "Scottish Terrier", 15, "Cookie");
+var beatrice = new ShowDog("Beatrice", "Pomeranian", 5, "Hamilton");
 
 spot.bark = function() {
     console.log(this.name + " says WOOF!");
@@ -61,5 +88,30 @@ barnaby.sit();
 spot.sit();
 spot.sit();
 
-console.log(spot.hasOwnProperty("species"));
-console.log(fido.hasOwnProperty("species"));
+scotty.stack();
+scotty.bark();
+console.log(scotty.league);
+console.log(scotty.species);
+
+scotty.bark();
+beatrice.bark();
+
+scotty.gait("Walk");
+beatrice.groom();
+
+if (fido instanceof Dog) {
+    console.log("Fido is a Dog");
+}
+if (fido instanceof ShowDog) {
+    console.log("Fido is a ShowDog");
+}
+
+if (scotty instanceof Dog) {
+    console.log("Scotty is a Dog");
+}
+if (scotty instanceof ShowDog) {
+    console.log("Scotty is a ShowDog");
+}
+
+console.log("Fido constructor is " + fido.constructor);
+console.log("Scotty constructor is " + scotty.constructor);
